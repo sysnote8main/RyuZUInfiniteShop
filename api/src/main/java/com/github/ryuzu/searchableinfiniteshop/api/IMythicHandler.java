f437f284347f233c0da55e586b615d527e72c9f8
Y-RyuZU <ryuzu1006@gmail.com>
2023-04-19 18:02:10 +0900

    multi version

---
+++ api/src/main/java/com/github/ryuzu/searchableinfiniteshop/api/IMythicHandler.java
@@ -0,0 +1,26 @@
+package com.github.ryuzu.searchableinfiniteshop.api;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+
+
+public interface IMythicHandler {
+    String getID(ItemStack item);
+
+    void reload(Consumer<Runnable> consumer);
+
+    boolean exsistsMythicMob(String id);
+
+    Collection<String> getMythicMobs();
+
+    ItemStack getMythicItem(String id, int amount);
+
+    Entity spawnMythicMob(String id, Location location);
+
+    boolean isMythicMob(Entity entity);
+}
