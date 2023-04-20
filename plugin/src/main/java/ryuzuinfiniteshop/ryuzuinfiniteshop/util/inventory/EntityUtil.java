5215d8ee540308d5469f67aa0b9e8889f9ef05d4
Y_RyuZU <ryuzu1006@gmail.com>
2023-04-20 21:00:34 +0900

    複数バージョン完全対応

--- plugin/src/main/java/ryuzuinfiniteshop/ryuzuinfiniteshop/util/inventory/EntityUtil.java
+++ plugin/src/main/java/ryuzuinfiniteshop/ryuzuinfiniteshop/util/inventory/EntityUtil.java
@@ -1,7 +1,14 @@
 package ryuzuinfiniteshop.ryuzuinfiniteshop.util.inventory;
 
+import org.bukkit.Bukkit;
 import org.bukkit.DyeColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class EntityUtil {
@@ -11,4 +18,50 @@ public class EntityUtil {
                 DyeColor.values()[0] :
                 DyeColor.values()[nextindex];
     }
+
+    public static org.bukkit.entity.Entity spawnEntity(Location location, EntityType entityType, CreatureSpawnEvent.SpawnReason spawnReason) {
+        try {
+            // エンティティのクラスを取得する
+            Class<?> entityClass = Class.forName("net.minecraft.server." + getVersion() + ".Entity" + entityType.getName());
+
+            // エンティティのコンストラクタを取得する
+            Constructor<?> entityConstructor = entityClass.getConstructor(Class.forName("net.minecraft.server." + getVersion() + ".EntityTypes"), Class.forName("net.minecraft.server." + getVersion() + ".World"));
+
+            // ワールドのインスタンスを取得する
+            Object world = location.getWorld().getClass().getMethod("getHandle").invoke(location.getWorld());
+
+            // エンティティのインスタンスを生成する
+            Object entity = entityConstructor.newInstance(getEntityTypes(entityType), world);
+
+            // エンティティの位置を設定する
+            entity.getClass().getMethod("setLocation", double.class, double.class, double.class, float.class, float.class).invoke(entity, location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+
+            // エンティティをスポーンさせる
+            world.getClass().getMethod("addEntity", entityClass, Class.forName("net.minecraft.server." + getVersion() + ".EnumMobSpawnType")).invoke(world, entity, getSpawnType(spawnReason));
+
+            // スポーンしたエンティティのBukkitのEntityオブジェクトを返す
+            return Bukkit.getEntity(((Entity) entity).getUniqueId());
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static String getVersion() {
+        String packageName = Bukkit.getServer().getClass().getPackage().getName();
+        return packageName.substring(packageName.lastIndexOf('.') + 1);
+    }
+
+    private static Object getEntityTypes(EntityType entityType) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
+        Class<?> entityTypesClass = Class.forName("net.minecraft.server." + getVersion() + ".EntityTypes");
+        return entityTypesClass.getMethod("a", Class.forName("net.minecraft.server." + getVersion() + ".EntityTypes")).invoke(null, entityTypesClass.getField(entityType.name()).get(null));
+    }
+
+    private static Object getSpawnType(CreatureSpawnEvent.SpawnReason spawnReason) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        Class<?> enumSpawnType = Class.forName("net.minecraft.server." + getVersion() + ".EnumMobSpawnType");
+        return enumSpawnType.getField(spawnReason.name()).get(null);
+    }
+
 }
